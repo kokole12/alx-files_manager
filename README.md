783a7af4acfa6ca0d632b3d7dbca8cc5c2d5d74d
kokole12 <kokoleismail886@gmail.com>
2023-08-31 15:17:58 +0300

    redis.js file created

---
+++ README.md
@@ -0,0 +1,1 @@
+# ALX Trimester summary
---
+++ utils/redis.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+const { createClient } = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => console.log(err));
+  }
+
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(key);
+    return value;
+  }
+
+  async set(key, value, time) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    await setAsync(key, value);
+    await this.client.expire(key, time);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
